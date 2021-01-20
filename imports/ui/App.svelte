69e00ccb3847bba101c14c3df3f35994fc23d4cd
freeseamew <freeseamew@gmail.com>
2021-01-21 00:55:34 +0900

    no message

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,16 +1,16 @@
-<!-- <script>
+<script>
   import { Router } from "@roxi/routify";
   import { routes } from "@roxi/routify/tmp/routes";
   // import { routes } from "../../.routify/routes.js";
 </script>
 
 <Router {routes} />
- -->
 
-<script>
+
+<!-- <script>
   import About from './pages/about.svelte'
   import Posts from './pages/posts.svelte'
 </script>
 
 <About />
-<Posts />
+<Posts /> -->
